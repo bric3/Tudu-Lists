2f4a87579a2000a01c809169f7649120bd064469
unknown <BRiCE@.(none)>
2011-06-03 23:58:45 +0200

    updated Admin Controller tests with docs and some more mockito usage

--- src/main/java/tudu/web/mvc/AdministrationController.java
+++ src/main/java/tudu/web/mvc/AdministrationController.java
@@ -30,6 +30,17 @@ public class AdministrationController {
 
     /**
      * Show the administration page action.
+     *
+     * Niveau 1
+     * - Vérifier qu'aucune interactions n'a lieu lorsque la page demandée n'est ni "configuration" ni "users"
+     *
+     * Niveau 2
+     * - La réponse par défaut du mock configService et faire un test avec la page configuration
+     * - Vérifier dans un test que pour la page "configuration" configService soit invoqué quelque soit les propriétés
+     *
+     * Niveau 2 à 3
+     * - Vérifier dans un test que pour la page "configuration" les propriétés smtp (et uniquement celles là) soit donnée au model
+     * - Vérifier dans un test que pour la page "configuration" il n'y a pas d'interaction avec userService.
      */
     @RequestMapping(method = RequestMethod.GET)
     public ModelAndView display(@RequestParam(required = false) String page) {
@@ -79,6 +90,18 @@ public class AdministrationController {
 
     /**
      * Update the application configuration.
+     *
+     * Niveau 1
+     * - Vérifer que pour l'action "enableUser" le service afférent est appelé et que disableUser ne l'est pas
+     * - Vérifer que pour l'action "disableUser" le service afférent est appelé et que enableUser ne l'est pas (d'une manière différente)
+     *
+     * Niveau 2
+     * - Vérifier que pour l'action appelle findUsersByLogin après un enableUser ou un disableUser
+     * - Vérifier que pour l'action appelle findUsersByLogin après un enableUser ou un disableUser
+     *
+     * Niveau 2 à 3
+     * - Vérifier qu'aucune intéraction n'est faite avec userService/ configurationService si la page ne correspond à rien
+     * - Vérifier que le configService.updateEmailProperties est bien appelé en ne vérifiant que les valeurs user et password
      */
     @RequestMapping(method = RequestMethod.POST)
     public ModelAndView update(@ModelAttribute AdministrationModel model) {
--- src/test/java/tudu/web/mvc/AdministrationControllerTest.java
+++ src/test/java/tudu/web/mvc/AdministrationControllerTest.java
@@ -3,8 +3,10 @@ package tudu.web.mvc;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InOrder;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.exceptions.verification.SmartNullPointerException;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.web.servlet.ModelAndView;
 import tudu.domain.Property;
@@ -12,6 +14,7 @@ import tudu.service.ConfigurationService;
 import tudu.service.UserService;
 
 import static org.fest.assertions.Assertions.assertThat;
+import static org.mockito.Answers.RETURNS_SMART_NULLS;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.willReturn;
 import static org.mockito.Matchers.anyString;
@@ -20,22 +23,43 @@ import static org.mockito.Mockito.*;
 @RunWith(MockitoJUnitRunner.class)
 public class AdministrationControllerTest {
 
-    @Mock private ConfigurationService cfgService;
+    @Mock(answer = RETURNS_SMART_NULLS) private ConfigurationService cfgService;
     @Mock private UserService userService;
 
-    @InjectMocks private AdministrationController admnController = new AdministrationController();
+    @InjectMocks private AdministrationController adminController = new AdministrationController();
 
     @Test
     public void display_should_return_a_nonnull_model() throws Exception {
-        assertThat(admnController.display("somepage")).isNotNull();
+        assertThat(adminController.display("somepage")).isNotNull();
         // don't break; didn't try hard enough
     }
 
     @Test
     public void display_should_not_interact_when_page_different_than_configuration_or_users() throws Exception {
-        assertThat(admnController.display("somepage")).isNotNull();
+        assertThat(adminController.display("somepage")).isNotNull();
 
         verifyZeroInteractions(cfgService, userService);
+    }
+
+    @Test(expected = SmartNullPointerException.class)
+    public void display_should_trhow_mockito_ex_when_configService_is_accessed_properties_when_page_is_configuration() throws Exception {
+        // given config service not stubbed
+
+        // when
+        ModelAndView mv = adminController.display("configuration");
+    }
+
+    @Test
+    public void display_should_read_configService_properties_when_page_is_configuration() throws Exception {
+        // given
+        given(cfgService.getProperty(anyString())).willReturn(property("whatever"));
+
+        // when
+        ModelAndView mv = adminController.display("configuration");
+
+        // then
+        verifyZeroInteractions(userService);
+        assertThat(mv.getModelMap().get("page")).isEqualTo("configuration");
     }
 
     @Test
@@ -51,13 +75,10 @@ public class AdministrationControllerTest {
 
 
         // when
-        ModelAndView mv = admnController.display("configuration");
+        ModelAndView mv = adminController.display("configuration");
 
 
         // then
-        verifyZeroInteractions(userService);
-        assertThat(mv.getModelMap().get("page")).isEqualTo("configuration");
-
         AdministrationModel adminModel = (AdministrationModel) mv.getModelMap().get("administrationModel");
         assertThat(adminModel.getSmtpHost()).isEqualTo("the host");
         assertThat(adminModel.getSmtpPort()).isEqualTo("the port");
@@ -71,17 +92,19 @@ public class AdministrationControllerTest {
     public void update_shouldnt_return_a_null_model() throws Exception {
         AdministrationModel adminModel = new AdministrationModel();
         adminModel.setAction("whatever");
-        assertThat(admnController.update(adminModel));
+        assertThat(adminController.update(adminModel));
     }
 
     @Test
-    public void update_should_update_smtp_config() throws Exception {
+    public void update_should_update_smtp_config_and_nothing_else() throws Exception {
         // given
-        AdministrationController spiedAdmnController = spy(admnController);
+        AdministrationController spiedAdmnController = spy(adminController);
         willReturn(new ModelAndView()).given(spiedAdmnController).display(anyString());
+
         AdministrationModel adminModel = new AdministrationModel();
         adminModel.setAction("configuration");
-
+        adminModel.setSmtpUser("the user");
+        adminModel.setSmtpPassword("password");
 
         // when
         spiedAdmnController.update(adminModel);
@@ -90,8 +113,8 @@ public class AdministrationControllerTest {
         verify(cfgService).updateEmailProperties(
                 anyString(),
                 anyString(),
-                anyString(),
-                anyString(),
+                eq("the user"),
+                eq("password"),
                 anyString()
         );
 
@@ -104,9 +127,10 @@ public class AdministrationControllerTest {
         adminModel.setAction("enableUser");
         adminModel.setLogin("Paterne");
 
-        admnController.update(adminModel);
+        adminController.update(adminModel);
 
         verify(userService).enableUser("Paterne");
+        verify(userService, never()).disableUser("Paterne");
     }
 
     @Test
@@ -115,20 +139,23 @@ public class AdministrationControllerTest {
         adminModel.setAction("disableUser");
         adminModel.setLogin("Bob");
 
-        admnController.update(adminModel);
+        adminController.update(adminModel);
 
         verify(userService).disableUser("Bob");
+        verify(userService, times(0)).enableUser("Bob");
     }
 
     @Test
-    public void update_should_fetch_users_on_login_() throws Exception {
+    public void update_should_fetch_users_on_login_after_disabling_suer() throws Exception {
         AdministrationModel adminModel = new AdministrationModel();
         adminModel.setAction("disableUser");
-        adminModel.setLogin("Bob");
+        adminModel.setSearchLogin("bob*");
 
-        admnController.update(adminModel);
+        adminController.update(adminModel);
 
-        verify(userService).disableUser("Bob");
+        InOrder inOrder = inOrder(userService);
+        inOrder.verify(userService).disableUser(anyString());
+        inOrder.verify(userService).findUsersByLogin("bob*");
     }
 
     private Property property(String value) {
